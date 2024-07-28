a7765349407fd2e7c0f70e3d686b8665db02d585
Andrea Nisticò <andrea.nistic@gmail.com>
2024-07-28 20:08:32 +0200

    Fixes third party lib and clang formatting

--- game/apps/main.cpp
+++ game/apps/main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <string>
+#include <string_view>
 
 #include "httplib.h"
+
+std::string game_state_json = R"({
+    "number_of_players": 2,
+    "players": [
+        {
+            "name": "Alice",
+            "hand": [5, 10, 15]
+        },
+        {
+            "name": "Bob",
+            "hand": [3, 8, 13]
+        }
+    ],
+    "decks": {
+        "ascending_deck": [1, 2, 3, 4, 5],
+        "descending_deck": [100, 99, 98, 97, 96]
+    },
+    "current_turn": "Alice"
+})";
 
 int main()
 {
   // HTTP
   httplib::Server svr;
 
-  svr.Get("/hi",
+  svr.Get("/get_state",
           [](const httplib::Request&, httplib::Response& res)
           {
-            res.set_content("Hello World!", "text/plain");
+            std::string_view sw{game_state_json};
+            res.set_content(sw.data(), "text/plain");
             std::cout << "The Game !! " << std::endl;
           });
 
--- game/utils/third-party/cpp_httplib.BUILD
+++ game/utils/third-party/cpp_httplib.BUILD
@@ -3,12 +3,13 @@ load("@rules_cc//cc:defs.bzl", "cc_library")
 cc_library(
     name = "cpp_httplib",
     srcs = glob([
-        "cpp_httplib/*.cpp",
-        "cpp_httplib/*.h",
+        "*.cpp",
+        "*.h",
     ]),
     hdrs = glob([
-        "cpp_httplib/*.h",
+        "*.h",
     ]),
     includes = ["."],
+    linkopts = ["-lpthread"],
     visibility = ["//visibility:public"],
 )
