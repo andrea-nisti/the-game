fd9a87c1c78b3b2c7e8b3d8d4b5f89928b6882f1
Andrea Nisticò <andrea.nistic@gmail.com>
2024-07-29 22:42:44 +0200

    Adds packaging rules and Docker composing script

---
+++ game/apps/deploy/cook-image.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Usage function
+usage() {
+    echo "Usage: $0 -t <tag> -f <tar_file>"
+    echo "  -t <tag>        Docker image tag"
+    echo "  -f <tar_file>   Name of the tar file in the Bazel exec root (e.g., bazel-bin/apps/game.tar)"
+    echo "  -h print help"
+    exit 1
+}
+
+# Parse command-line arguments
+while getopts "t:f:h" opt; do
+    case $opt in
+    t)
+        TAG=$OPTARG
+        echo image tag: $TAG
+        ;;
+    f)
+        TAR_FILE=$OPTARG
+        echo file: $TAR_FILE
+        ;;
+    h)
+        usage
+        ;;
+    esac
+done
+
+# Check if both arguments are provided
+if [ -z "$TAG" ] || [ -z "$TAR_FILE" ]; then
+    usage
+fi
+
+# Variables
+# Get the directory containing the script
+
+SCRIPT_DIR=$(dirname $(realpath "$0"))
+BAZEL_EXEC_ROOT=$(bazel info execution_root)
+DEST_DIR=$SCRIPT_DIR
+DOCKERFILE="Dockerfile"
+# BASE_PATH = "../../bazel-bin/apps"
+
+# Copy the tar file from Bazel exec root to the destination directory
+cp "$(realpath -m "$SCRIPT_DIR/../../bazel-bin/apps/$TAR_FILE")" "$DEST_DIR/"
+
+# Ensure the Dockerfile exists in the destination directory
+if [ ! -f "$DOCKERFILE" ]; then
+    echo "Dockerfile not found!"
+    exit 1
+fi
+
+# Change to the destination directory
+cd $DEST_DIR
+
+# Build the Docker image
+docker build -t game_image:$TAG .
+
+# Print success message
+echo "Docker image built successfully: game_image:$TAG"
