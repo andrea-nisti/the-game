26a05c55111ed7cb472b461708b5bfec490cb6be
Andrea Nisticò <andrea.nistic@gmail.com>
2024-08-30 03:02:17 +0200

    Adds http session docs

--- game/support/networking/http_session.h
+++ game/support/networking/http_session.h
@@ -11,7 +11,6 @@
 #include <boost/optional.hpp>
 #include <boost/throw_exception.hpp>
 
-#include "net_utils.hpp"
 #include "session_base.h"
 
 namespace beast = boost::beast;    // from <boost/beast.hpp>
@@ -22,31 +21,105 @@ using tcp = boost::asio::ip::tcp;  // from <boost/asio/ip/tcp.hpp>
 namespace game::support
 {
 
+/**
+ * \addtogroup support Support
+ * \brief
+ * @{
+ */
+
+/**
+ * \addtogroup networking Networking Utilities
+ * \brief Utility classes and functions for network operations
+ * \ingroup support
+ * @{
+ */
+
+/**
+ * @class HttpSession
+ * @brief Represents an HTTP session.
+ *
+ * This class provides a way to handle HTTP requests and responses.
+ * It inherits from SessionBase and provides additional functionality
+ * specific to HTTP sessions.
+ *
+ * @extends SessionBase<beast::tcp_stream, beast::flat_buffer>
+ * @extends std::enable_shared_from_this<HttpSession>
+ */
 class HttpSession final : public SessionBase<beast::tcp_stream, beast::flat_buffer>,
                           public std::enable_shared_from_this<HttpSession>
 {
   public:
+    /**
+     * @brief Constructor. Creates a new HttpSession object.
+     *
+     * @param socket The socket to use for the session.
+     */
     HttpSession(tcp::socket&& socket)
         : SessionBase<beast::tcp_stream, beast::flat_buffer>(std::move(socket))
     {
     }
 
+    /**
+     * @brief Destructor. Closes the session.
+     */
     virtual ~HttpSession() { Close(); }
 
+    /**
+     * @brief Starts the session and begins processing requests.
+     */
     void Run() override;
 
-  private:
-    void OnRead(boost::system::error_code ec, std::size_t bytes_transferred) override;
-    void OnWrite(boost::system::error_code ec, std::size_t bytes_transferred) override;
-
-    void Read() override;
-    void Write() override;
+    /**
+     * @brief Closes the session and releases any resources.
+     */
     void Close() override;
 
+    /**
+     * @brief Reads data from the socket and processes any requests.
+     */
+    void Read() override;
+
+    /**
+     * @brief Writes data to the socket by sending responses.
+     */
+    void Write() override;
+
+    /**
+     * @brief This method is called when data is read from the session.
+     *
+     * @param ec The error code.
+     * @param bytes_transferred The number of bytes transferred.
+     */
+    void OnRead(boost::system::error_code ec, std::size_t bytes_transferred) override;
+
+    /**
+     * @brief Called when data is written to the session.
+     *
+     * @param ec The error code.
+     * @param bytes_transferred The number of bytes transferred.
+     */
+    void OnWrite(boost::system::error_code ec, std::size_t bytes_transferred) override;
+
+  private:
+    /**
+     * @brief The parser for the session.
+     */
     boost::optional<http::request_parser<http::string_body>> parser_;
+
+    /**
+     * @brief Whether to keep the session alive, set by the client. If false, the session
+     * will be closed.
+     */
     bool keep_alive_ {false};
+
+    /**
+     * @brief The response for the session.
+     */
     http::response<http::string_body> response_;
 };
+
+/// @}
+/// @}
 
 }  // namespace game::support
 
