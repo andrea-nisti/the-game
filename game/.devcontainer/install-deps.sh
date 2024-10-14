97c2c1eb03d90a1f8cdeaecf8b0afca6ba94e78b
Andrea Nisticò <andrea.nistic@gmail.com>
2024-10-15 00:53:55 +0200

    Updates dev files

--- game/.devcontainer/install-deps.sh
+++ game/.devcontainer/install-deps.sh
@@ -7,7 +7,13 @@ BIN_DIR=$HOME/.local/bin
 if [ ! -d "$BIN_DIR" ]; then
     mkdir -p $BIN_DIR
 fi
+cd $BIN_DIR
 
 # Download bazel buildifier
-wget -O $BIN_DIR/buildifier https://github.com/bazelbuild/buildtools/releases/download/v7.3.1/buildifier-linux-amd64
-chmod +x $BIN_DIR/buildifier
+wget -O buildifier https://github.com/bazelbuild/buildtools/releases/download/v7.3.1/buildifier-linux-amd64
+chmod +x buildifier
+
+# Download bazel lsp
+wget -O starpls.tar.gz https://github.com/withered-magic/starpls/releases/download/v0.1.14/starpls-linux-amd64.tar.gz
+tar -xf starpls.tar.gz && rm -rf starpls.tar.gz
+chmod +x starpls
