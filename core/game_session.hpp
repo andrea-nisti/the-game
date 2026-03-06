d698ca5ee68b98ece52799c7ffd107949128eead
Andrea Nisticò <andrea.nistic@gmail.com>
2026-03-06 13:44:07 +0100

    cleanup session

--- core/game_session.hpp
+++ core/game_session.hpp
@@ -19,15 +19,12 @@ class PlayerControllerBase
 };
 
 template <typename Command>
-class GameSession
+class GameSession : public std::enable_shared_from_this<GameSession<Command>>
 {
   public:
     using PlayerControllerPtr = std::shared_ptr<PlayerControllerBase<Command>>;
 
-    explicit GameSession(boost::asio::io_context& io_ctx)
-        : command_strand_(boost::asio::make_strand(io_ctx)), player_strand_(boost::asio::make_strand(io_ctx))
-    {
-    }
+    explicit GameSession(boost::asio::io_context& io_ctx) : strand_(boost::asio::make_strand(io_ctx)) {}
     GameSession(const GameSession&) = delete;
     GameSession(GameSession&&) = delete;
     auto operator=(const GameSession&) -> GameSession& = delete;
@@ -37,41 +34,41 @@ class GameSession
 
     void OnCommand(const Command& command, PlayerControllerPtr sender)
     {
-        boost::asio::post(command_strand_, [this, command, sender]() { ProcessCommand(command, sender); });
+        boost::asio::post(strand_, [this, command, sender]() { ProcessCommand(command, sender); });
     }
 
     void AddPlayer(PlayerControllerPtr player)
     {
-        boost::asio::post(player_strand_, [this, player]() {
+        boost::asio::post(strand_, [this, player]() {
             // Avoid duplicate players
-            auto it = std::find(players_.begin(), players_.end(), player);
-            if (it != players_.end())
+            auto it = std::find(player_controllers_.begin(), player_controllers_.end(), player);
+            if (it != player_controllers_.end())
             {
                 return;
             }
-            players_.push_back(player);
-            boost::asio::post(command_strand_, [this, player]() { OnPlayerJoined(player); });
+            player_controllers_.push_back(player);
+            boost::asio::post(strand_, [this, player]() { OnPlayerJoined(player); });
         });
     }
 
     void RemovePlayer(PlayerControllerPtr player)
     {
-        boost::asio::post(player_strand_, [this, player]() {
-            auto it = std::find(players_.begin(), players_.end(), player);
-            if (it != players_.end())
+        boost::asio::post(strand_, [this, player]() {
+            auto it = std::find(player_controllers_.begin(), player_controllers_.end(), player);
+            if (it != player_controllers_.end())
             {
-                players_.erase(it);
+                player_controllers_.erase(it);
                 // Notify after erase so the player is no longer in the list
                 // when OnPlayerLeft runs on the command strand
-                boost::asio::post(command_strand_, [this, player]() { OnPlayerLeft(player); });
+                boost::asio::post(strand_, [this, player]() { OnPlayerLeft(player); });
             }
         });
     }
 
     void BroadcastMessage(const std::string& message, PlayerControllerPtr except = nullptr)
     {
-        boost::asio::post(player_strand_, [this, message, except]() {
-            for (auto& player : players_)
+        boost::asio::post(strand_, [this, message, except]() {
+            for (auto& player : player_controllers_)
             {
                 if (player != except)
                 {
@@ -84,23 +81,23 @@ class GameSession
     /// @brief Disconnects and removes all players, then calls OnShutdown.
     void Shutdown()
     {
-        boost::asio::post(player_strand_, [this]() {
-            for (auto& player : players_)
+        boost::asio::post(strand_, [this]() {
+            for (auto& player : player_controllers_)
             {
                 player->Disconnect();
             }
-            players_.clear();
-            boost::asio::post(command_strand_, [this]() { OnShutdown(); });
+            player_controllers_.clear();
+            boost::asio::post(strand_, [this]() { OnShutdown(); });
         });
     }
 
-    /// @brief Returns the number of connected players. Safe to call only from player_strand_.
-    [[nodiscard]] std::size_t GetPlayerCount() const { return players_.size(); }
+    /// @brief Returns the number of connected players. Safe to call only from strand_.
+    [[nodiscard]] std::size_t GetPlayerCount() const { return player_controllers_.size(); }
 
-    /// @brief Check if a player is in the session. Safe to call only from player_strand_.
+    /// @brief Check if a player is in the session. Safe to call only from strand_.
     [[nodiscard]] bool HasPlayer(const PlayerControllerPtr& player) const
     {
-        return std::find(players_.begin(), players_.end(), player) != players_.end();
+        return std::find(player_controllers_.begin(), player_controllers_.end(), player) != player_controllers_.end();
     }
 
   protected:
@@ -110,9 +107,8 @@ class GameSession
     virtual void OnShutdown() {}
 
   private:
-    boost::asio::strand<boost::asio::io_context::executor_type> command_strand_;
-    boost::asio::strand<boost::asio::io_context::executor_type> player_strand_;
-    std::vector<PlayerControllerPtr> players_;
+    boost::asio::strand<boost::asio::io_context::executor_type> strand_;
+    std::vector<PlayerControllerPtr> player_controllers_;
 };
 
 }  // namespace game::core
